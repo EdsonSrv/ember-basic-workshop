b3ecf3b5b6165d663802c24376b6b381ae780963
EdsonSrv <edson@ciencias.unam.mx>
2019-05-06 09:51:04 -0500

    Finding Models

--- app/models/person.js
+++ app/models/person.js
@@ -5,3 +5,10 @@ export default DS.Model.extend({
   age: DS.attr('number'),
   admin: DS.attr('boolean')
 });
+
+//let person = this.get('store').findRecord('person', 1); // => GET /persons/1 by Path Variable
+
+//let person = this.get('store').findRecord('person', {"username":"carlogilmar"}); // => GET by Query Params
+
+//let people = this.get('store').findAll('person'); // => GET /persons
+
